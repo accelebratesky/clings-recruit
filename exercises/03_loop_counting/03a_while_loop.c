1033835bd4b07f4fe1d123a91c9a915f6cba7c01
idontknow <idontknow@192.168.237.128>
2026-08-20 21:31:45 +0800

    完成03a while循环数到10

--- exercises/03_loop_counting/03a_while_loop.c
+++ exercises/03_loop_counting/03a_while_loop.c
@@ -14,9 +14,11 @@
  */
 
 #include <stdio.h>
-
 int main(void) {
-#error TODO: Fix this exercise. Run "clings hint" for help.
-
+int counter=0;
+while (counter < 10){
+    counter++;
+    printf("counter = %d\n",counter);
+}
     return 0;
 }
